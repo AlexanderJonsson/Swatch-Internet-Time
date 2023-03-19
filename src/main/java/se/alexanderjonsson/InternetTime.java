93ab677125f49b0448bae7270122ef0d4183f6b7
Alexander Jonsson <alexander.jonsson@hey.com>
2023-03-19 19:41:11 +0100

    Added docstrings

--- src/main/java/se/alexanderjonsson/InternetTime.java
+++ src/main/java/se/alexanderjonsson/InternetTime.java
@@ -2,31 +2,58 @@ package se.alexanderjonsson;
 
 import java.time.LocalTime;
 import java.time.ZoneId;
-
 public final class InternetTime {
+    /**
+     * This method returns the current time in Swatch .beats as an integer derived from the
+     * following formula: (currentTimeInSeconds / 86400 * 1000).
+     * @return an Integer representing the current time in .beats
+     */
     public static Integer getCurrentTimeAsInteger() {
-        long currentTimeInMillis = getCurrentTimeInZurichAsSeconds();
-        double swatchTime = (double) currentTimeInMillis / 86400 * 1000;
+        long currentTimeInSeconds = getCurrentTimeInZurichAsSeconds();
+        double swatchTime = (double) currentTimeInSeconds / 86400 * 1000;
         return (int) Math.round(swatchTime);
     }
 
+    /**
+     * This method returns the current time in Swatch .beats as a Double derived from the
+     * following formula: (currentTimeInSeconds / 86400 * 1000).
+     * @return a Double representing the current time in .beats
+     */
     public static Double getCurrentTimeAsDouble() {
-        long currentTimeInMillis = getCurrentTimeInZurichAsSeconds();
-        return (double) currentTimeInMillis / 86400 * 1000;
+        long currentTimeInSeconds = getCurrentTimeInZurichAsSeconds();
+        return (double) currentTimeInSeconds / 86400 * 1000;
     }
 
+    /**
+     * This method returns the current time in Swatch .beats as a String derived from the
+     * following formula: (currentTimeInSeconds / 86400 * 1000).
+     * The string can be formatted by supplying the desired TimeFormat enum
+     *
+     * @param format select the desired TimeFormat enum to pick how the string is formatted
+     * @return a string formatted according to the chosen parameter.
+     */
     public static String getCurrentTimeAsString(TimeFormat format) {
         switch (format){
             case WITH_CENTIBEATS: return String.format("%06.2f",getCurrentTimeAsDouble()).replace(",",".");
             case WITHOUT_CENTIBEATS: return String.format("%03d",getCurrentTimeAsInteger()).replace(",",".");
             case WITHOUT_CENTIBEATS_WITHOUT_LEADING_ZEROES: return String.format("%s",getCurrentTimeAsInteger()).replace(",",".");
             case CENTIBEATS_WITHOUT_LEADING_ZEROES: return String.format("%.2f",getCurrentTimeAsDouble()).replace(",",".");
-            default: return null;
+            default: return getCurrentTimeAsString();
         }
     }
+
+    /**
+     * This method overloads getCurrentTimeAsString(TimeFormat format)
+     * @return a String representing the current time in .beats
+     */
     public static String getCurrentTimeAsString() {
         return getCurrentTimeAsString(TimeFormat.WITH_CENTIBEATS);
     }
+
+    /**
+     * This method returns the current time in Zurich as number of seconds since midnight in order to be used in the Swatch Internet Time calculations
+     * @return the number of seconds since midnight in Zurich
+     */
     private static long getCurrentTimeInZurichAsSeconds() {
         ZoneId timezone = ZoneId.of("Europe/Zurich");
         LocalTime now = LocalTime.now(timezone);
@@ -34,6 +61,10 @@ public final class InternetTime {
         return now.toSecondOfDay() - midnight.toSecondOfDay();
     }
 
+    /**
+     *
+     * @return a pretty string representation of the current time in .beats.
+     */
     @Override
     public String toString() {
         return String.format("@%s .beats", getCurrentTimeAsString(TimeFormat.WITH_CENTIBEATS));
