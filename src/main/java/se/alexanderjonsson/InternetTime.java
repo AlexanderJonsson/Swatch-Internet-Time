3feb33d4b1bc972ce69778188e9f814e8da74989
Alexander Jonsson <39478379+AlexanderJonsson@users.noreply.github.com>
2023-03-19 13:52:56 +0100

    initial commit

---
+++ src/main/java/se/alexanderjonsson/InternetTime.java
@@ -0,0 +1,41 @@
+package se.alexanderjonsson;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+
+public final class InternetTime {
+    public static Integer getCurrentTimeAsInteger() {
+        long currentTimeInMillis = getCurrentTimeInZurichAsSeconds();
+        double swatchTime = (double) currentTimeInMillis / 86400 * 1000;
+        return (int) Math.round(swatchTime);
+    }
+
+    public static Double getCurrentTimeAsDouble() {
+        long currentTimeInMillis = getCurrentTimeInZurichAsSeconds();
+        return (double) currentTimeInMillis / 86400 * 1000;
+    }
+
+    public static String getCurrentTimeAsString(TimeFormat format) {
+        switch (format){
+            case WITH_CENTIBEATS: return String.format("%06.2f",getCurrentTimeAsDouble()).replace(",",".");
+            case WITHOUT_CENTIBEATS: return String.format("%03d",getCurrentTimeAsInteger()).replace(",",".");
+            case WITHOUT_CENTIBEATS_WITHOUT_LEADING_ZEROES: return String.format("%s",getCurrentTimeAsInteger()).replace(",",".");
+            case CENTIBEATS_WITHOUT_LEADING_ZEROES: return String.format("%.2f",getCurrentTimeAsDouble()).replace(",",".");
+            default: return null;
+        }
+    }
+    public static String getCurrentTimeAsString() {
+        return getCurrentTimeAsString(TimeFormat.WITH_CENTIBEATS);
+    }
+    private static long getCurrentTimeInZurichAsSeconds() {
+        ZoneId timezone = ZoneId.of("Europe/Zurich");
+        LocalTime now = LocalTime.now(timezone);
+        LocalTime midnight = LocalTime.MIDNIGHT;
+        return now.toSecondOfDay() - midnight.toSecondOfDay();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("@%s .beats", getCurrentTimeAsString(TimeFormat.WITH_CENTIBEATS));
+    }
+}
